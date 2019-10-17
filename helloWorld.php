43e6fd97d27eb343737bd5a625060fd88d8d752e
Bgirlplusplus <wi17b021@technikum-wien.at>
2019-10-17 23:17:39 +0200

    helloWorld V4

--- helloWorld.php
+++ helloWorld.php
@@ -2,15 +2,7 @@
 
 echo 'Hello World';
 echo 'This is real life';
-<<<<<<< HEAD
+
 $counter=0;
 ?>
-=======
-$newVariable=0;
-$array=array('sunshine', 'overcast', 'rainy', 'thunder and lightning');
-foreach($array as $weather){
-  $newVariable=$newVariable+1;
-}
 
-?>
->>>>>>> master
